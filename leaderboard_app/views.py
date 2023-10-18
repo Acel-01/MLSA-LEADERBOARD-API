a960764899dc7f71b8dcf144144d3b9640a77168
Acel-01 <emekaaladimma@gmail.com>
2023-10-18 10:25:54 +0100

    fix: nginx and error handling

--- leaderboard_app/views.py
+++ leaderboard_app/views.py
@@ -38,71 +38,79 @@ class SubmitCreateView(APIView):
         description="Submit a Pull Request",
     )
     def post(self, request):
-        # Remove trailing slash do that all pr_link entries are uniform
-        if request.data.get("pr_link")[-1] == "/":
-            request.data["pr_link"] = request.data["pr_link"][:-1]
+        try:
+            # Remove trailing slash do that all pr_link entries are uniform
+            if request.data.get("pr_link")[-1] == "/":
+                request.data["pr_link"] = request.data["pr_link"][:-1]
 
-        serializer = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
-        serializer.is_valid(raise_exception=True)
+            serializer = self.serializer_class(
+                data=request.data, context={"request": request}
+            )
+            serializer.is_valid(raise_exception=True)
 
-        user_url = serializer.initial_data.get("pr_link")
-        list_url = user_url.split("/")
+            user_url = serializer.initial_data.get("pr_link")
+            list_url = user_url.split("/")
 
-        if (
-            list_url[0] != "https:"
-            or list_url[2] != "github.com"
-            or list_url[5] != "pull"
-        ):
+            if (
+                list_url[0] != "https:"
+                or list_url[2] != "github.com"
+                or list_url[5] != "pull"
+            ):
+                raise serializers.ValidationError({"pr_link": ["This link is invalid"]})
+
+            if Submit.objects.filter(user=request.user, pr_link=user_url):
+                raise serializers.ValidationError(
+                    {"pr_link": ["You have submitted this PR before"]}
+                )
+
+            # https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER
+            api_url = f"https://api.github.com/repos/{list_url[3]}/{list_url[4]}/pulls/{list_url[6]}"
+            r = requests.get(api_url)
+
+            if r.status_code == 404:
+                raise serializers.ValidationError(
+                    {
+                        "pr_link": [
+                            "Invalid! This PR is from a private repository or it does not exist."
+                        ]
+                    }
+                )
+
+            if self.request.user.username != r.json().get("user").get("login"):
+                raise serializers.ValidationError(
+                    {
+                        "pr_link": [
+                            "Your Github username does not match this Pull Request"
+                        ]
+                    }
+                )
+
+            # https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/merge
+            api_url = f"https://api.github.com/repos/{list_url[3]}/{list_url[4]}/pulls/{list_url[6]}/merge"
+            r = requests.get(api_url)
+
+            if r.status_code == 204:
+                # PR was merged
+                points = 4
+            elif r.status_code == 404:
+                # PR was not merged
+                points = 2
+
+            request.user.total_points += points
+
+            # Add or Update this leaders score on the leaderboard
+            redis_mlsa_leaderboard.rank_member_in(
+                leaderboard_name=leaderboard_name,
+                member=request.user.username,
+                score=request.user.total_points,
+            )
+
+            request.user.save()
+            serializer.save(points=points, user=self.request.user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(f"something broke, haha. Here's the error\n{e}")
             raise serializers.ValidationError({"pr_link": ["This link is invalid"]})
-
-        if Submit.objects.filter(user=request.user, pr_link=user_url):
-            raise serializers.ValidationError(
-                {"pr_link": ["You have submitted this PR before"]}
-            )
-
-        # https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER
-        api_url = f"https://api.github.com/repos/{list_url[3]}/{list_url[4]}/pulls/{list_url[6]}"
-        r = requests.get(api_url)
-
-        if r.status_code == 404:
-            raise serializers.ValidationError(
-                {
-                    "pr_link": [
-                        "Invalid! This PR is from a private repository or it does not exist."
-                    ]
-                }
-            )
-
-        if self.request.user.username != r.json().get("user").get("login"):
-            raise serializers.ValidationError(
-                {"pr_link": ["Your Github username does not match this Pull Request"]}
-            )
-
-        # https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/merge
-        api_url = f"https://api.github.com/repos/{list_url[3]}/{list_url[4]}/pulls/{list_url[6]}/merge"
-        r = requests.get(api_url)
-
-        if r.status_code == 204:
-            # PR was merged
-            points = 4
-        elif r.status_code == 404:
-            # PR was not merged
-            points = 2
-
-        request.user.total_points += points
-
-        # Add or Update this leaders score on the leaderboard
-        redis_mlsa_leaderboard.rank_member_in(
-            leaderboard_name=leaderboard_name,
-            member=request.user.username,
-            score=request.user.total_points,
-        )
-
-        request.user.save()
-        serializer.save(points=points, user=self.request.user)
-        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
 class LeaderboardView(APIView):
